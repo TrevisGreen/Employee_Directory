b0ed8e095d014ac1d83747f2071a03bc2611c0b2
TrevisGreen <Trevis.Green@gmail.com>
2023-04-18 15:47:26 -0500

    Setup fields, constructors, getters/setters

--- src/main/java/com/myapps/employeedirectory/model/Employee.java
+++ src/main/java/com/myapps/employeedirectory/model/Employee.java
@@ -1,4 +1,70 @@
 package com.myapps.employeedirectory.model;
 
 public class Employee {
+
+    // Define fields
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+
+    // Define constructors
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // Define getter/setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Define toString
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
