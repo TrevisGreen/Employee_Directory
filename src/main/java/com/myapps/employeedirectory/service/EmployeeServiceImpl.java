9216ec77596c027a7352287828ca389249c39cb3
TrevisGreen <Trevis.Green@gmail.com>
2023-04-18 16:18:24 -0500

    created appropriate methods

--- src/main/java/com/myapps/employeedirectory/service/EmployeeServiceImpl.java
+++ src/main/java/com/myapps/employeedirectory/service/EmployeeServiceImpl.java
@@ -1,4 +1,50 @@
 package com.myapps.employeedirectory.service;
 
-public class EmployeeServiceImpl {
+import com.myapps.employeedirectory.dao.EmployeeRepository;
+import com.myapps.employeedirectory.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    // Using constructor injection
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+        employeeRepository = theEmployeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Optional<Employee> result = employeeRepository.findById(theId);
+        Employee theEmployee = null;
+        if(result.isPresent()) {
+            theEmployee = result.get();
+        } else {
+            // didn't find the employee
+            throw new RuntimeException("Did not find employee id " + theId);
+        }
+
+        return theEmployee;
+    }
+
+    @Override
+    public void save(Employee theEmployee) {
+        employeeRepository.save(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        employeeRepository.deleteById(theId);
+    }
 }
