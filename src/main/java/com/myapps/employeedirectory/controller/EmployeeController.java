11021b8a200a69f78e04900e6b8a7ad797572d92
TrevisGreen <Trevis.Green@gmail.com>
2023-04-18 18:15:19 -0500

    fixed bug

--- src/main/java/com/myapps/employeedirectory/controller/EmployeeController.java
+++ src/main/java/com/myapps/employeedirectory/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.myapps.employeedirectory.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -43,7 +40,7 @@ public class EmployeeController {
         return "employees/employee-form";
     }
 
-    @GetMapping("/save")
+    @PostMapping("/save")
     public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
         // Save the employee
         employeeService.save(theEmployee);
