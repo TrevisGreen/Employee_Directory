8102bec1e5e03de5472bf7eeabe2b7686993ad5a
TrevisGreen <Trevis.Green@gmail.com>
2023-04-18 16:05:12 -0500

    Added methods

--- src/main/java/com/myapps/employeedirectory/service/EmployeeService.java
+++ src/main/java/com/myapps/employeedirectory/service/EmployeeService.java
@@ -1,4 +1,16 @@
 package com.myapps.employeedirectory.service;
 
+import com.myapps.employeedirectory.model.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    void save(Employee theEmployee);
+
+    void delete(int theId);
 }
