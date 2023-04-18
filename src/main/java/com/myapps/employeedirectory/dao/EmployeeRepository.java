fdb8785ddf11ac9dbef742991b8a2ebbe01b674f
TrevisGreen <Trevis.Green@gmail.com>
2023-04-18 16:01:44 -0500

    Set up the Jpa repository

--- src/main/java/com/myapps/employeedirectory/dao/EmployeeRepository.java
+++ src/main/java/com/myapps/employeedirectory/dao/EmployeeRepository.java
@@ -1,4 +1,9 @@
 package com.myapps.employeedirectory.dao;
 
-public interface EmployeeRepository {
+import com.myapps.employeedirectory.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // That's it ... no more code for now!!
 }
