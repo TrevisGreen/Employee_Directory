70fbc842e7573cc14d2b2c03be0065650be94f8c
TrevisGreen <Trevis.Green@gmail.com>
2023-04-18 17:13:16 -0500

    add method for list employees

--- src/main/java/com/myapps/employeedirectory/controller/EmployeeController.java
+++ src/main/java/com/myapps/employeedirectory/controller/EmployeeController.java
@@ -1,12 +1,36 @@
 package com.myapps.employeedirectory.controller;
 
+import com.myapps.employeedirectory.model.Employee;
+import com.myapps.employeedirectory.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
+    private EmployeeService employeeService;
 
+    @Autowired
+    public EmployeeController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
+    }
+
+    // add mapping for "/list"
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+        // get the employees from db
+        List<Employee> theEmployees = employeeService.findAll();
+
+        // add to the spring model
+        theModel.addAttribute("employees", theEmployees);
+
+        return "employees/list-employees";
+    }
 
 }
